f66c770ea71f975ea00f5285e495d1749114aa44
xovel <xovel@vip.qq.com>
2017-02-06 17:46:20 +0800

    indent using spaces

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
 gulp.task('clean', function(){
-	return gulp.src('./dist')
-		.pipe(clean());
+  return gulp.src('./dist')
+    .pipe(clean());
 });
 
 gulp.task('default', ['clean'], function() {
-	gulp.src('sinput.js').pipe(gulp.dest('./dist/'));
+  gulp.src('sinput.js').pipe(gulp.dest('./dist/'));
   return gulp.src('sinput.js')
     .pipe(uglify())
     .pipe(rename('sinput.min.js'))
