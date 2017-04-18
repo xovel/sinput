ecfb823d58c26298a22c0c95e68ff63e3f6acb26
xovel <xovel@vip.qq.com>
2017-04-18 22:52:08 +0800

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,23 @@ var clean = require('gulp-clean');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
-gulp.task('clean', function(){
+gulp.task('clean', function () {
   return gulp.src('./dist')
     .pipe(clean());
 });
 
-gulp.task('default', ['clean'], function(){
-  gulp.src('sinput.js').pipe(gulp.dest('./dist/'));
+gulp.task('default', ['clean'], function () {
   return gulp.src('sinput.js')
+    .pipe(gulp.dest('./dist/'))
     .pipe(uglify({preserveComments: 'license'}))
     .pipe(concat('sinput.min.js'))
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('build', ['clean'], function(){
-  gulp.src(['sinput.js', 'config.js'])
-    .pipe(concat('sinput.js'))
-    .pipe(gulp.dest('./dist/'));
+gulp.task('build', ['clean'], function () {
   return gulp.src(['sinput.js', 'config.js'])
+    .pipe(concat('sinput.js'))
+    .pipe(gulp.dest('./dist/'))
     .pipe(concat('sinput.min.js'))
     .pipe(uglify({preserveComments: 'license'}))
     .pipe(gulp.dest('./dist/'));
